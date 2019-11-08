55a0dfc2bd58826720aad0c156a6f3ec8d401e3b
starrysky0501 <1070186459@qq.com>
2019-11-08 23:29:29 +0800

    牛客：排序子序列

---
+++ test_11_8/test_11_8/practice.cpp
@@ -0,0 +1,42 @@
+//排序子序列
+//题目理解：递减是a[i]>a[i+1];则非递减就是a[i]<=a[i+1]
+//			递增是a[i]<a[i+1];则非递增就是a[i]>=a[i+1]
+//该题的思路：找到波峰和波谷的个数，之后+1
+//需要注意：1.当该序列的个数为奇数时，则会直接跳过(i==n-3)，永远不会执行else语句；相当于for循环判断出波峰和波谷的个数;
+//			最后+1就是有序子序列；当该序列的个数为偶数时，则会到达(i==n-3)条件，会执行else里面的break;跳出for循环，
+//          因为n-3包括该位置之前的数据都已判断，所以数组中只剩两个数字，则直接为1个有序序列。
+//          2.else语句不可省略,当序列为1,2,1,2,1,2,1,2 时，如果没有break语句，则会使最终值多1。
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdlib.h>
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main()
+//{
+//	int n = 0;
+//	vector<int> v;
+//	int sum = 1;
+//	cin >> n;
+//	for (int i = 0; i < n; i++)
+//	{
+//		int a;
+//		cin >> a;
+//		v.push_back(a);   // 将输入的数字放在数组里
+//	}
+//	for (int i = 1; i<n - 1; i++)
+//	{
+//		if ((v[i] > v[i - 1] && v[i]>v[i + 1]) ||
+//			(v[i] < v[i - 1] && v[i] <v[i + 1]))    //找到波峰或波谷
+//		{
+//			++sum;
+//			if (i != n - 3)
+//				++i;
+//			else
+//				break;
+//		}
+//	}
+//	cout << sum << endl;
+//	system("pause");
+//	return 0;
+//}
