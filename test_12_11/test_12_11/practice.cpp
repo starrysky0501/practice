6669b9a372d093080d6a68e99ed4105ec7be0f09
starrysky0501 <1070186459@qq.com>
2019-12-11 22:26:17 +0800

    牛客：triangle

---
+++ test_12_11/test_12_11/practice.cpp
@@ -0,0 +1,34 @@
+﻿//牛客：triangle
+
+//给出一个三角形，计算从三角形顶部到底部的最小路径和，每一步都可以移动到下面一行相邻的数字，
+//例如，给出的三角形如下：
+//[↵[2], ↵[3, 4], ↵[6, 5, 7], ↵[4, 1, 8, 3]↵]
+//最小的从顶部到底部的路径和是2 + 3 + 5 + 1 = 11。
+
+
+//class Solution {
+//public:
+//	int minimumTotal(vector<vector<int> > &triangle) {
+//		vector<vector<int>> minsum(triangle);
+//		int row = minsum.size();
+//		for (int i = 1; i<row; i++)
+//		{
+//			for (int j = 0; j <= i; j++)
+//			{
+//				if (j == 0)
+//					minsum[i][j] = minsum[i - 1][j];
+//				else if (j == i)
+//					minsum[i][j] = minsum[i - 1][j - 1];
+//				else
+//					minsum[i][j] = min(minsum[i - 1][j], minsum[i - 1][j - 1]);
+//				minsum[i][j] = minsum[i][j] + triangle[i][j];
+//			}
+//		}
+//		int allsum = minsum[row - 1][0];
+//		for (int i = 1; i<row; i++)
+//		{
+//			allsum = min(allsum, minsum[row - 1][i]);
+//		}
+//		return allsum;
+//	}
+//};
