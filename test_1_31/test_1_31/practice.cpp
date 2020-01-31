78ab0529d9b64425cd0d6d872a645564b973b974
starrysky0501 <1070186459@qq.com>
2020-01-31 23:14:56 +0800

    2018校招真题：彩色的砖块

---
+++ test_1_31/test_1_31/practice.cpp
@@ -0,0 +1,21 @@
+//2018校招真题：彩色的砖块
+
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//int main()
+//{
+//	string str;
+//	int res = 0;
+//	cin >> str;
+//	sort(str.begin(), str.end());
+//	//cout << str << endl;
+//	str.erase(unique(str.begin(), str.end()), str.end());
+//	if (str.size() == 1)
+//		res = 1;
+//	else if (str.size() == 2)
+//		res = 2;
+//	cout << res << endl;
+//	return 0;
+//}
