8ebc5c190d7eb63542d89d08f46da87969c84694
starrysky0501 <1070186459@qq.com>
2020-02-02 21:16:03 +0800

    2018校招真题：交错01串

---
+++ test_2_2/test_2_2/practice.cpp
@@ -0,0 +1,28 @@
+//2018校招真题：交错01串
+
+//#include <iostream>
+//#include <string.h>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	cin >> s;
+//	int length = 1;
+//	int temp = 1;
+//	for (int i = 0; i<s.size() - 1; i++)
+//	{
+//		if (s[i] != s[i + 1])
+//		{
+//			length++;
+//			if (temp<length)
+//			{
+//				temp = length;
+//			}
+//		}
+//		else
+//		{
+//			length = 1;
+//		}
+//	}
+//	cout << temp << endl;
+//}
