27b0512240030612ab83df85012620d75a1a13e4
starrysky0501 <1070186459@qq.com>
2020-02-03 22:07:29 +0800

    2018校招真题：操作序列

---
+++ test_2_3/test_2_3/practice.cpp
@@ -0,0 +1,38 @@
+//2018校招真题：操作序列
+
+//#include <iostream>
+//#include <string.h>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cin >> n;
+//	int a[n];
+//	for (int i = 0; i<n; i++)
+//	{
+//		cin >> a[i];
+//	}
+//	if (n % 2 == 0)
+//	{
+//		for (int j = n - 1; j>0; j = j - 2)
+//		{
+//			cout << a[j] << " ";
+//		}
+//		for (int k = 0; k<n; k = k + 2)
+//		{
+//			cout << a[k] << " ";
+//		}
+//	}
+//	else
+//	{
+//		for (int l = n - 1; l >= 0; l = l - 2)
+//		{
+//			cout << a[l] << " ";
+//		}
+//		for (int k = 1; k<n - 1; k = k + 2)
+//		{
+//			cout << a[k] << " ";
+//		}
+//	}
+//
+//}
