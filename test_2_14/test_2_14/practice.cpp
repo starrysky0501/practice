1336c84ed61605f6633664e2dd4cb1213855ec16
starrysky0501 <1070186459@qq.com>
2020-02-14 22:24:02 +0800

    2018校招真题：排序

---
+++ test_2_14/test_2_14/practice.cpp
@@ -0,0 +1,27 @@
+//2018–£’–’ÊÃ‚£∫≈≈–Ú
+
+//#include<iostream>
+//#include<algorithm>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	int num, x;
+//	cin >> num;
+//	vector<int> nums, temp;
+//	while (num--)
+//	{
+//		cin >> x;
+//		nums.push_back(x);
+//		temp.push_back(x);
+//	}
+//	sort(nums.begin(), nums.end());
+//	int cnt = 0;
+//	for (int i = 0; i < nums.size(); i++)
+//	{
+//		if (nums[i] != temp[i])
+//			cnt++;
+//	}
+//	cout << cnt << endl;
+//	return 0;
+//}
