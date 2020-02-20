c3ce9793a78b95a18990c5e09bc676cb0d4f594e
starrysky0501 <1070186459@qq.com>
2020-02-20 20:38:58 +0800

    2018校招真题：红和绿

---
+++ test_2_20/test_2_20/practice.cpp
@@ -0,0 +1,28 @@
+//2018校招真题：红和绿
+
+//#include<bits/stdc++.h>
+//using namespace std;
+//
+//int main()
+//{
+//	std::ios::sync_with_stdio(false);
+//	string s;
+//	cin >> s;
+//	int len = s.size();// 获取字符串长度
+//	int gCount = 0;// 字符串中G字母的个数
+//	int count = 0; // 最小涂色次数
+//	for (int i = 0; i < len; i++)
+//	{
+//		if (s[i] == 'G')
+//		{
+//			gCount++;
+//		}
+//		else
+//		{
+//			count = min(gCount, count + 1);
+//		}
+//
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
