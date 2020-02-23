c8c3b452e7a92b06ac31116209eb6d102f0d2741
starrysky0501 <1070186459@qq.com>
2020-02-23 19:32:59 +0800

    2018校招真题：循环数比较

---
+++ test_2_23/test_2_23/practice.cpp
@@ -0,0 +1,26 @@
+//2018校招真题：循环数比较
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main(){
+//	string str1, str2, out1, out2;
+//	int k1, k2;
+//	cin >> str1 >> k1 >> str2 >> k2;
+//	if (str1.size()*k1<str2.size()*k2)
+//		cout << "Less" << endl;
+//	else if (str1.size()*k1>str2.size()*k2)
+//		cout << "Greater" << endl;
+//	else{
+//		for (int i = 0; i<k1; i++)
+//			out1 += str1;
+//		for (int i = 0; i<k2; i++)
+//			out2 += str2;
+//		if (out1 == out2)
+//			cout << "Equal" << endl;
+//		else if (out1<out2)
+//			cout << "Less" << endl;
+//		else
+//			cout << "Greater" << endl;
+//	}
+//}
