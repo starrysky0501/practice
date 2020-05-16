72fc41bcf031d3f7ef3299062c974a6fbcbaa654
starrysky0501 <1070186459@qq.com>
2020-05-16 23:08:13 +0800

    2018校招真题：疯狂序列

---
+++ test_5_16/test_5_16/practice.cpp
@@ -0,0 +1,19 @@
+//2018校招真题：疯狂序列
+//题目描述
+//东东从京京那里了解到有一个无限长的数字序列 : 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, ...(数字k在该序列中正好出现k次)。东东想知道这个数字序列的第n项是多少, 你能帮帮他么
+//输入描述 :
+//输入包括一个整数n(1 ≤ n ≤ 10 ^ 18)
+//输出描述 :
+//输出一个整数, 即数字序列的第n项
+//#include<iostream>
+//#include<cmath>
+//using namespace std;
+//int main(){
+//	long long int n;
+//	cin >> n;
+//	long long int num;
+//	long double x = sqrt(2 * n + 1 / 4) - 1.0 / 2;
+//	num = x + 1;
+//	cout << num << endl;
+//	return 0;
+//}
