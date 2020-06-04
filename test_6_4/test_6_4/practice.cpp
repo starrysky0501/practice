cc5241c04439a07f775dbd013140eaeef71dcba8
starrysky0501 <1070186459@qq.com>
2020-06-04 23:22:51 +0800

    2018校招真题：超链接

---
+++ test_6_4/test_6_4/practice.cpp
@@ -0,0 +1,28 @@
+//2018校招真题：超链接
+//#define _CRT_SECURE_NO_DEPRECATE
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <map>
+//using namespace std;
+//map<string, int> mymap;
+//int main(){
+//	int n;
+//	scanf("%d%*c", &n);
+//	string s;
+//	for (int i = 0; i<n; ++i){
+//		cin >> s;
+//		mymap[s] = 1;
+//	}
+//	scanf("%d%*c", &n);
+//	for (int i = 0; i<n; ++i){
+//		cin >> s;
+//		mymap.erase(s);
+//	}
+//
+//	map<string, int>::iterator iter;
+//	for (iter = mymap.begin(); iter != mymap.end(); ++iter){
+//		cout << iter->first << endl;
+//	}
+//
+//}
