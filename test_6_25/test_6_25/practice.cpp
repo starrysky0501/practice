969784a747cc23d12e4737cbf70cedd27ded66d3
starrysky0501 <1070186459@qq.com>
2020-06-25 22:09:08 +0800

    2018校招真题：两个整数二进制位不同个数

---
+++ test_6_25/test_6_25/practice.cpp
@@ -0,0 +1,22 @@
+//题目描述
+//输入两个整数，求两个整数二进制格式有多少个位不同
+//输入描述 :
+//两个整数
+//输出描述 :
+//二进制不同位的个数
+
+//#include<iostream> 
+//using namespace std;
+//int main(){
+//	int a, b, i = 0;
+//	cin >> a >> b;
+//	a = a^b;
+//	while (a != 0){
+//		if (a & 1 == 1) {
+//			++i;
+//		}
+//		a = a >> 1;
+//	}
+//	cout << i << endl;
+//	return 0;
+//}
