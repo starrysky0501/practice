329cee61392b6fe1bd2e61c82ad2dbc59482fd91
starrysky0501 <1070186459@qq.com>
2020-07-05 22:37:58 +0800

    2018校招真题：返回小于N的质数个数

---
+++ test_7_5/test_7_5/practice.cpp
@@ -0,0 +1,33 @@
+//题目描述
+//请考虑性能
+//输入描述 :
+//一个整数N
+//输出描述 :
+//小于N的质数数量
+
+//#include<iostream> 
+//#include<cmath>
+//using namespace std;
+//bool isPrimer(int x);
+//int main()
+//{
+//	int N;
+//	while (cin >> N){
+//		int count = 0;
+//		for (int i = 2; i<N; i++){
+//			if (isPrimer(i))
+//				count++;
+//		}
+//		cout << count << endl;
+//	}
+//	return 0;
+//}
+//bool isPrimer(int x)
+//{
+//	bool flag = true;
+//	for (int i = 2; i <= sqrt(x); i++){
+//		if (x%i == 0)
+//			flag = false;
+//	}
+//	return flag;
+//}
