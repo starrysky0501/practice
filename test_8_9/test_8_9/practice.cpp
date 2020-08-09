40df83daf163b75d3e8c166f3ae1f41193878531
starrysky0501 <1070186459@qq.com>
2020-08-09 23:50:31 +0800

    2018校招真题：整数反转

---
+++ test_8_9/test_8_9/practice.cpp
@@ -0,0 +1,20 @@
+//题目描述
+//输入一个有符号整数，输出该整数的反转值。
+//输入描述 :
+//一个整数
+//输出描述 :
+//一个整数
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int input;
+//	cin >> input;
+//	int output = 0;
+//	while (input != 0)
+//	{
+//		output = output * 10 + input % 10;
+//		input /= 10;
+//	}
+//	cout << output;
+//}
